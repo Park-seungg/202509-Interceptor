d68f6a6225efd723bbe8dd024617a57e80671468
Park-seungg <parkis1101@gmail.com>
2025-08-27 11:46:29 +0900

    t3 - 게시물 생성

--- src/main/java/com/back/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.java
@@ -1,27 +1,38 @@
 package com.back.domain.post.post.repository;
 
 import com.back.domain.post.post.dto.Post;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface PostRepository {
     @Select("""
-    <script>
-    SELECT * 
-    FROM post
-    </script>
-    """)
+            <script>
+            SELECT * 
+            FROM post
+            </script>
+            """)
     List<Post> findAll();
 
     @Select("""
-    <script>
-    SELECT *
-    FROM post
-    WHERE id = #{id}
-    </script>
-    """)
+            <script>
+            SELECT *
+            FROM post
+            WHERE id = #{id}
+            </script>
+            """)
     Post findById(int id);
+
+    @Insert("""
+            <script>
+            INSERT INTO post
+            set createDate = NOW(),
+            modifyDate = NOW(),
+            title = #{title},
+            content = #{content}
+            </script>
+            """)
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int create(Post post);
 }
